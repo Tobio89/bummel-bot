124943f2602420775ea17cb89d97c4aeae39ee2a
Tobias S <44494591+BruceJi7@users.noreply.github.com>
2021-11-24 15:27:42 +0900

    Remove config

--- config/config.go
+++
@@ -1,5 +0,0 @@
-package config
-
-const Key = "Njk3NzgwMDAwNTg5MzQ4ODY2.Xo8QRA.fxVLryMpTkWunpSvJjtLYfOEai8"
-const GuildID = "588878518511992842"
-const BotShitCHannel = "botshit"
--- eventHandlers/eventHandlers.go
+++ eventHandlers/eventHandlers.go
@@ -3,6 +3,7 @@ package eventHandlers
 import (
 	"fmt"
 
+	"github.com/BruceJi7/bummel-bot/config"
 	"github.com/BruceJi7/bummel-bot/eventHandlers/events"
 	"github.com/bwmarrin/discordgo"
 )
@@ -11,5 +12,24 @@ func AddEventHandlers(dg *discordgo.Session) {
 	fmt.Println("Adding events...")
 	fmt.Println("OnReady")
 	dg.AddHandler(events.OnReady)
+
+}
+
+func CreateCommands(dg *discordgo.Session) {
+
+	_, err := dg.ApplicationCommandCreate(config.AppID, config.GuildID, commands.EraseCommand)
+	if err != nil {
+		fmt.Println("Error adding erase command:")
+		fmt.Println(err)
+	} else {
+		fmt.Println("Erase command added")
+	}
+	_, err = dg.ApplicationCommandCreate(config.AppID, config.GuildID, commands.ForceLogCommand)
+	if err != nil {
+		fmt.Println("Error adding forcelog command:")
+		fmt.Println(err)
+	} else {
+		fmt.Println("Forcelog command added")
+	}
 
 }
